59d9446de9ee2b4f4b76b89d323d24fc2f840da8
GlaDdos <kamil.kawa@gmail.com>
2019-01-05 20:13:20 +0100

    Added compositor class for displaying layers with graphics, done some refractoring

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,15 @@
 import { loadLevel } from './loaders.js';
 import { loadMarioSprite, loadBackgroundSprites} from './sprites.js';
+import Compositor  from './Compositor.js';
+import {createBackgroundLayer} from './layers.js';
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-function drawBackground(background, context, sprites) {
-  background.ranges.forEach( ([x1, x2, y1, y2]) => {
-    for( let x = x1; x < x2; x++) {
-      for( let y = y1; y < y2; y++) {
-        sprites.drawTile(background.tile, context, x, y);
-      }
-    }
-  });
+function createSpriteLayer(sprite, pos) {
+  return function drawSpriteLayer(contex) {
+    sprite.draw('idle', context, pos.x, pos.y);
+  };
 }
 
 Promise.all([
@@ -19,10 +17,27 @@ Promise.all([
   loadBackgroundSprites(),
   loadLevel('1-1')
 ])
-.then(([mario, sprites, level] ) => {
-    level.backgrounds.forEach(background => {
-      drawBackground(background, context, sprites);
-    });
+.then(([marioSprites, backgroundSprites, level] ) => {
+    const comp = new Compositor();
 
-    mario.draw('idle', context, 64, 64);
-})
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+    comp.layers.push(backgroundLayer);
+
+    const pos = {
+      x: 64, y: 64
+    }
+
+    const spriteLayer = createSpriteLayer(marioSprites, pos);
+    comp.layers.push(spriteLayer);
+
+    function update() {
+      comp.draw(context);
+
+      pos.x += 2;
+      pos.y += 2;
+
+      requestAnimationFrame(update);
+    }
+
+    update();
+});
