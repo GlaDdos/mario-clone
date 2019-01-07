d10f96cbe4e2dd7a701db68727f4a02c777ad468
GlaDdos <kamil.kawa@gmail.com>
2019-01-07 21:17:12 +0100

    Timer class added

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ import { loadBackgroundSprites} from './sprites.js';
 import Compositor  from './Compositor.js';
 import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 import { createMario } from './entities.js';
+import Timer from './Timer.js';
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
@@ -16,21 +17,26 @@ Promise.all([
 ])
 .then(([mario, backgroundSprites, level] ) => {
     const comp = new Compositor();
-    const gravity = 0.5;
+    
+    const gravity = 30;
+
+    const timer = new Timer();
+
+    mario.position.set(64, 180);
+    mario.velocity.set(200, -600)
 
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-    //comp.layers.push(backgroundLayer);
+    comp.layers.push(backgroundLayer);
 
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
 
-    function update() {
-      comp.draw(context);
-      mario.velocity.y += gravity;
-      mario.update();
+    timer.update = function update(deltaTime) {
+        comp.draw(context);
+        mario.velocity.y += gravity;
+        mario.update(deltaTime);
+      }
 
-      requestAnimationFrame(update);
+      timer.start();
     }
-
-    update();
-});
+);
--- public/js/math.js
+++ public/js/math.js
@@ -2,6 +2,7 @@ export class Vec2 {
   constructor(x, y) {
     this.set(x, y);
   }
+  
   set(x, y) {
     this.x = x;
     this.y = y;
